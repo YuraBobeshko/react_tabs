93f7cf91f1e014b4d2aa601d094d1867e8c814d6
YuraBobeshko <gorobech55@gmail.com>
2019-10-31 22:17:43 +0200

    Updates

---
+++ precache-manifest.f1f314aaad34de987f97a91e4ad5e3ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ecd25e752674e0a93b461de5427487",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "0d82c5be7dd4f093746e",
+    "url": "/react_tabs/static/css/main.b9c01be5.chunk.css"
+  },
+  {
+    "revision": "a16792bbd021bec6ef7e",
+    "url": "/react_tabs/static/js/2.27899654.chunk.js"
+  },
+  {
+    "revision": "0d82c5be7dd4f093746e",
+    "url": "/react_tabs/static/js/main.ba443e14.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
